7d7c97dc75834f63b724f0467fe2c5f51a6e0f26
Tariq DataWala <56953139+tariq-datawala@users.noreply.github.com>
2022-09-14 17:11:51 +0500

    maintain calculator

--- calculator/dist/index.js
+++ calculator/dist/index.js
@@ -1,4 +1,7 @@
 import * as inquirer from "inquirer";
+let num1;
+let num2;
+let currentOperator;
 function isNumber(str) {
     const mayBeNum = parseInt(str);
     const isNum = !isNaN(mayBeNum);
@@ -16,16 +19,12 @@ function isOperator(operator) {
             return false;
     }
 }
-let num1;
-let num2;
-let currentOperator;
 function firstInput() {
     const prompt = inquirer.createPromptModule();
     prompt({ type: "input", name: "num1", message: "Enter first Number:" })
         .then(answer => {
         if (answer["num1"] !== "") {
             const isNum = isNumber(answer["num1"]);
-            // console.log(isNum)
             if (isNum) {
                 num1 = parseInt(answer["num1"]);
                 operator();
@@ -35,7 +34,6 @@ function firstInput() {
                 firstInput();
             }
         }
-        // SecondInput();
     });
 }
 function SecondInput() {
@@ -48,7 +46,6 @@ function SecondInput() {
                 num2 = parseInt(answer["num2"]);
                 const result = Calculator(num1, num2, currentOperator);
                 console.log(`Your Result is ::: ${num1} ${currentOperator} ${num2} = ${result}`);
-                // console.log(isNum) 
             }
             else {
                 console.log("Invalid Input...");
@@ -75,17 +72,8 @@ function operator() {
                 operator();
             }
         }
-        // console.log(num1,num2)
     });
 }
-firstInput();
-// function cal(){
-//     firstInput();
-//     operator();
-//     SecondInput();
-//     console.log(`first Number is ${num1}/n second numberis ${num2} and operator is ${currentOperator}`)
-// }
-// cal()
 function Calculator(firstNumber, secondNumber, operator) {
     switch (operator) {
         case "+":
@@ -98,3 +86,4 @@ function Calculator(firstNumber, secondNumber, operator) {
             return firstNumber / secondNumber;
     }
 }
+firstInput();
